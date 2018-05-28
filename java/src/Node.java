badcc6f35121e2e5621090ef78154ff61f98e0f2
yannic <yannickuester@gmail.com>
2018-05-28 11:52:06 +0200

    added local ip recognition to preprocessor

--- java/src/Node.java
+++ java/src/Node.java
@@ -1,7 +1,12 @@
 public class Node {
     public String ip;
-
+    public boolean local;
     public Node(String ip){
         this.ip = ip;
+        if(ip != null && (ip.startsWith("192.168.") || ip.startsWith("10.") || ip.matches("^172\\.[16..31].*$"))){
+            local = true;
+        }else {
+            local = false;
+        }
     }
 }
